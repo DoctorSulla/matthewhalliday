2a1cd314fe75a69e6120817389a48812d52d2420
DoctorSulla <matthew.halliday@gmail.com>
2025-09-05 17:50:13 +0100

    Commit to test deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,11 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav class="flex">
+<a class="text-violet-500 px-1" href="#">Home</a>
+<a  class="text-violet-500 px-1" href="#">Photos</a>
+<a class="text-violet-500 px-1" href="#">About Me</a>
+</nav>
+<main class="w-6xl m-auto">
 {@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-purple-500 text-center text-3xl">Matthew Halliday</h1>
+<img src="matt_no_bg.png" class="m-auto" style="width:20rem">
+<p>Welcome to my website. This is a space to share my work, musings and anything else I might find interesting. There isn't much here for the time-being but I will add more stuff as I have the time / motivation.</p>
+
