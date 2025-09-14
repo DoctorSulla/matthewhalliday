53e0a9dc408d5b524703f1db5a881212595320cb
Matthew Halliday <matthew.halliday@gmail.com>
2025-09-14 10:32:42 +0100

    Changed font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,11 @@
 	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
-<nav class="flex">
-	<a class="px-1 text-xl text-violet-500 hover:underline" href="/">Home</a>
-	<a class="px-1 text-xl text-violet-500 hover:underline" href="/photos">Photos</a>
-	<a class="px-1 text-xl text-violet-500 hover:underline" href="/about">About Me</a>
-	<a class="px-1 text-xl text-violet-500 hover:underline" href="/blog">Blog</a>
+<nav class="mb-2 flex bg-purple-50 py-2">
+	<a class="mukta-regular px-2 text-xl text-violet-500 hover:underline" href="/">Home</a>
+	<a class="mukta-regular px-2 text-xl text-violet-500 hover:underline" href="/photos">Photos</a>
+	<a class="mukta-regular px-2 text-xl text-violet-500 hover:underline" href="/about">About Me</a>
+	<a class="mukta-regular px-2 text-xl text-violet-500 hover:underline" href="/blog">Blog</a>
 </nav>
 <main class="m-auto max-w-6xl px-1">
 	{@render children?.()}
